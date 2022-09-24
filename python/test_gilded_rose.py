b75c325a51185d5940168b17478d268a3d6013cf
Susan Fung <fung_susan@hotmail.com>
2022-09-24 15:52:41 -0600

    Completed using Approval Tests.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -2,14 +2,14 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
-
+from approvaltests.approvals import verify
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("Quality degrades twice as fast", 0, 2), Item("Quality is never negative", 0, 0), Item("Aged Brie", 0, 0), Item("Quality never more than 50", 0, 52), Item("Sulfuras, Hand of Ragnaros", 0, 10), Item("Backstage passes to a TAFKAL80ETC concert", 12, 10), Item("Backstage passes to a TAFKAL80ETC concert", 10, 10), Item("Backstage passes to a TAFKAL80ETC concert", 4, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        verify(items)
 
         
 if __name__ == '__main__':

3534482c154236cfbfa91839bb57e9b6e5b6b4a8
Susan Fung <fung_susan@hotmail.com>
2022-12-26 14:58:39 -0700

    Moved create_items function outside of test class.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,13 +6,14 @@ from approvaltests import verify_all_combinations
 from gilded_rose import Item, GildedRose
 
 
+def create_items(name, sellIn, quality):
+    items = [Item(name, sellIn, quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    return items[0]
+
 class GildedRoseTest(unittest.TestCase):
     def test_quality_decreases_by_2_when_sellIn_less_than_or_equal_to_zero(self):
-        def create_items (name, sellIn, quality):
-            items = [Item(name, sellIn, quality)]
-            gilded_rose = GildedRose(items)
-            gilded_rose.update_quality()
-            return items[0]
         name = ["foo"]
         sellIn = [1, 0, -1]
         quality = [2]

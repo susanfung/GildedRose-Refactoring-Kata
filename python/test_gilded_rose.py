052c0ae09ad22fb40f08d87ea1e78bc69f7049b3
Susan Fung <fung_susan@hotmail.com>
2022-12-26 14:56:07 -0700

    Wrote test to test that quality decreases by two when sellIn value is less than or equal to zero.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from approvaltests import verify_all_combinations
+
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+    def test_quality_decreases_by_2_when_sellIn_less_than_or_equal_to_zero(self):
+        def create_items (name, sellIn, quality):
+            items = [Item(name, sellIn, quality)]
+            gilded_rose = GildedRose(items)
+            gilded_rose.update_quality()
+            return items[0]
+        name = ["foo"]
+        sellIn = [1, 0, -1]
+        quality = [2]
+        verify_all_combinations(lambda a, b, c: create_items(a, b, c), [name, sellIn, quality])
 
         
 if __name__ == '__main__':

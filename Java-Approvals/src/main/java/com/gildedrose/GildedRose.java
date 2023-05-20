847e951f8691ef2f10147873c2a3292d6aa528ec
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:55:39 -0600

    r Changed if condition to false

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -39,7 +39,7 @@ class GildedRose {
                 }
 
                 if (item.sellIn < 0) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (false) {
                     } else {
                         item.quality = 0;
                     }

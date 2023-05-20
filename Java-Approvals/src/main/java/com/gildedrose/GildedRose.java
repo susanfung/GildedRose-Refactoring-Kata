7cc0a9c82eaa086cd45dc427f13ee991e132dbf0
Susan Fung <fung_susan@hotmail.com>
2023-05-20 17:06:20 -0600

    r Removed empty if statement

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -20,12 +20,6 @@ class GildedRose {
             }
 
             if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        }
-                    }
-                }
                 continue;
             } else {
                 if (item.quality > 0) {

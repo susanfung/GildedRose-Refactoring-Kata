ec7de65c46c81457b37cf0cec8fde258dd13aa22
Susan Fung <fung_susan@hotmail.com>
2023-05-20 17:04:36 -0600

    r Removed red

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -31,7 +31,6 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
                         }
                     }
                 }

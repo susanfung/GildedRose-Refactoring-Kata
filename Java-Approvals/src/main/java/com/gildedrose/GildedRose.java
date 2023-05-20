eaba2b5c3dabe506d28a94def3b6c77718e18abe
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:39:38 -0600

    r Changed if condition

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -9,7 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (true) {
+            if (item.name.equals("Aged Brie")) {
                 appleSauce(item);
                 continue;
             } else {

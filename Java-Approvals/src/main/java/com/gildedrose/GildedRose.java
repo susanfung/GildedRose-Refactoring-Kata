bdc77efc4e242013fe5397f15f2528c4b7ccb420
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:57:12 -0600

    r Removed if statement

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -31,9 +31,7 @@ class GildedRose {
                     }
                 }
 
-                if (true) {
-                    item.sellIn = item.sellIn - 1;
-                }
+                item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
                     item.quality = 0;

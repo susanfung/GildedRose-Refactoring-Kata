2cfce59a020d0e067d1e5a21665c7568642b579f
Susan Fung <fung_susan@hotmail.com>
2023-05-20 17:54:57 -0600

    R Implement interface

--- Java-Approvals/src/main/java/com/gildedrose/BackstagePasses.java
+++ Java-Approvals/src/main/java/com/gildedrose/BackstagePasses.java
@@ -1,7 +1,8 @@
 package com.gildedrose;
 
-public class BackstagePasses {
+public class BackstagePasses implements ItemName {
 
+    @Override
     public void updateItem(Item item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
@@ -26,6 +27,7 @@ public class BackstagePasses {
         }
     }
 
+    @Override
     public boolean isItem(Item item) {
         return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }

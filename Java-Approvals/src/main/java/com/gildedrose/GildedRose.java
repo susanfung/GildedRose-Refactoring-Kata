203c2b52e2495af7e125dc1600ed6d675c658a88
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:41:14 -0600

    r Removed red

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -16,12 +16,6 @@ class GildedRose {
                         item.quality = item.quality + 1;
 
                         if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-
                             if (item.sellIn < 6) {
                                 if (item.quality < 50) {
                                     item.quality = item.quality + 1;

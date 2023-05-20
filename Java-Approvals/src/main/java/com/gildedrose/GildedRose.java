dc7ee54de60eb65d7da52f0c5e55dba02d15af16
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:56:38 -0600

    r Removed if statement

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -15,21 +15,18 @@ class GildedRose {
             }
 
             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (false) {
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }

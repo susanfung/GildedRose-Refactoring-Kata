53af9b3e792993c0f6c9bfcbcbec0997d568ac78
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:53:15 -0600

    r Removed red

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -40,11 +40,6 @@ class GildedRose {
 
                 if (item.sellIn < 0) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
                     } else {
                         item.quality = 0;
                     }

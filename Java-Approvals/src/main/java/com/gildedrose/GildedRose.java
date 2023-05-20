cdbe0d9d1c987dde7b8598e251c0cf2d1d625a2c
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:45:57 -0600

    r Changed if condition to true

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -45,7 +45,7 @@ class GildedRose {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
 
-                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (true) {
                             if (item.sellIn < 11) {
                                 if (item.quality < 50) {
                                     item.quality = item.quality + 1;

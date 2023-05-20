fce9c6fc98114188d59a44e02ed4a11331cb1e38
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:49:01 -0600

    r Unwrapped if statement

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -53,17 +53,14 @@ class GildedRose {
                 }
 
                 if (item.sellIn < 0) {
-                    if (true) {
-                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
                             }
-                        } else {
-                            item.quality = 0;
                         }
                     } else {
+                        item.quality = 0;
                     }
                 }
                 continue;

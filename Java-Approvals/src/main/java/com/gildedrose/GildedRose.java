b744bee02f82ad1d2d8571f74317875543c9ba07
Susan Fung <fung_susan@hotmail.com>
2023-05-20 16:48:08 -0600

    r Unwrapped if statement

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,9 +14,7 @@ class GildedRose {
                     item.quality = item.quality + 1;
                 }
 
-                if (true) {
-                    item.sellIn = item.sellIn - 1;
-                }
+                item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
                     if (item.quality < 50) {

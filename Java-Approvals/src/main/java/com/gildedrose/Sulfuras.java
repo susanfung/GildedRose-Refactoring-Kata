ca54589be98a2f36cb40979f3e622987f87d43cc
Susan Fung <fung_susan@hotmail.com>
2023-05-20 17:55:20 -0600

    R Implement interface

--- Java-Approvals/src/main/java/com/gildedrose/Sulfuras.java
+++ Java-Approvals/src/main/java/com/gildedrose/Sulfuras.java
@@ -1,11 +1,13 @@
 package com.gildedrose;
 
-public class Sulfuras {
+public class Sulfuras implements ItemName {
 
+    @Override
     public void updateItem(Item item) {
         return;
     }
 
+    @Override
     public boolean isItem(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }

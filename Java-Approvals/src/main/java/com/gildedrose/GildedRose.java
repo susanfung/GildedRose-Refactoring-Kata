7b443fa407776ed91a919f4de7e138e6bbb53d9d
Susan Fung <fung_susan@hotmail.com>
2023-05-20 17:39:46 -0600

    r Extract delegate

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -24,27 +24,9 @@ class GildedRose {
                 continue;
             }
 
-            if (isItem(item)) {
-                updateItem(item);
+            if (OtherItems.isItem(item)) {
+                OtherItems.updateItem(item);
                 continue;
-            }
-        }
-    }
-
-    private static boolean isItem(Item item) {
-        return true;
-    }
-
-    private static void updateItem(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
             }
         }
     }
---
+++ Java-Approvals/src/main/java/com/gildedrose/OtherItems.java
@@ -0,0 +1,21 @@
+package com.gildedrose;
+
+public class OtherItems {
+    public static boolean isItem(Item item) {
+        return true;
+    }
+
+    public static void updateItem(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+        }
+    }
+}
